bdf551b0d045bf53665c98d2bf5d6b4d4a2dc31a
neoqasim <neoqasim@gmail.com>
2023-10-12 21:45:06 +0500

    started ui

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+import Navbar from "./Navbar"
+import Quiz from "./Quiz"
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Quiz />
+    </>
+  )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <header>
+                <nav>
+                    <a href="#home">Home</a>
+                    <a href="#about">About</a>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Navbar
